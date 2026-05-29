b8ea26d5b4e60dff1e803d53083f2d794dd2cf79
Arjit Kumar <143163164+Arjit2716@users.noreply.github.com>
2026-05-29 17:22:02 +0530

    Time: 3 ms (26.37%) | Memory: 9.8 MB (9.98%) - LeetSync

--- 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.cpp
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.cpp
@@ -4,12 +4,12 @@ public:
         unordered_set<char>lower,upper;
         for(char ch:word){
             if(islower(ch)) lower.insert(ch);
-            else upper.insert(tolower(ch));
+            else upper.insert(ch);
         }
         int cnt=0;
         for(char ch:lower){
             
-            if(upper.count(ch)) cnt++;
+            if(upper.count(toupper(ch))) cnt++;
         }
         return cnt;
     }
